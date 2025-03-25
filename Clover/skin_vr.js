876f6f3880fcfac36e35582ecdb527a6cd47a89c
CAD-p-r-o-j-e-c-t <designofcad@yandex.ru>
2025-03-25 15:11:45 +0300

    3

--- Clover/skin_vr.js
+++ Clover/skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.1.8/20986
 // Filename: feather_vr.ggsk
-// Generated 2025-03-25T14:59:30
+// Generated 2025-03-25T15:10:41
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
