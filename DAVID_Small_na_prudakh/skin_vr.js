14bbacff5266d838323db58c70ed71a2188eb364
CAD-p-r-o-j-e-c-t <designofcad@yandex.ru>
2025-04-05 19:54:03 +0300

    Compilation

--- DAVID_Small_na_prudakh/skin_vr.js
+++ DAVID_Small_na_prudakh/skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.1.8/20986
 // Filename: feather_vr_Anatoliy.ggsk
-// Generated 2025-03-26T18:52:40
+// Generated 2025-04-05T19:48:59
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
